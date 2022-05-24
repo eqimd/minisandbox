66f48e87959d29c84bb532379d97edfa73d2676a
Krisha11 <vfhbyf2@yandex.ru>
2022-05-24 12:40:56 +0300

    example

--- tests/examples/wait/wait.cpp
+++ tests/examples/wait/wait.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 
 int main() {
-    fork();
-    while(true) {
-        std::cout << "Pid: " << getpid() << " doing something\n";
-        usleep(1000000);
+    pid_t pid = fork();
+    if (pid == 0) {
+        while(true) {
+            std::cout << "doing something\n";
+            usleep(1000000);
+        }
+    } else {
+        while(true) {
+            std::cout << "doing another something\n";
+            usleep(1000000);
+        }
     }
     return 0;
 }
